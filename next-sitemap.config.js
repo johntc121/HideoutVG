612c27a7ac7cf210bf0706b01729ad0ea6d0a45c
John Cripe <jcripe@webtraxllc.com>
2022-04-21 01:09:39 -0700

    added sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,16 @@
+let siteUrl = 'https://hideoutvg.com/';
+
+module.exports = {
+    siteUrl,
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [
+            { userAgent: "*", allow: "/"},
+        ],
+        additionalSitemaps: [
+            `${siteUrl}sitemap.xml`,
+            `${siteUrl}sitemap-0.xml`,
+        ],
+    },
+    sitemapSize: 999999999,
+}
--- test.js
+++
@@ -1,6 +0,0 @@
-var isPalindrome = function(x) {
-    let isPalindrome = x.toString().split("").join("") === x.toString().split("").reverse().join("")
-        ? true : false;
-
-    return isPalindrome;
-};
